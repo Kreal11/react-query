25de2fe9d8f72e3971103e736238e37eea34e6ad
Kreal11 <kirillshyrokov2@gmail.com>
2024-04-28 12:56:27 +0300

    added pagination by Load more
    + added showing of selected item by Id

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import Projects from "./components/Projects";
-import Todos from "./components/Todos";
+// import Projects from "./components/Projects";
+// import Todos from "./components/Todos";
+
+import Products from "./components/Products";
 
 const App = () => {
   return (
     <>
-      <Projects />
-      <Todos />
+      {/* <Projects />
+      <Todos /> */}
+      <Products />
     </>
   );
 };
